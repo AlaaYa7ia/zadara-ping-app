7144cbcade2d0306902184004ab88ebb42b06427
Alaa Yahia <alaa.yahia.1995@gmail.com>
2022-04-19 02:14:04 +0300

    Deploy app to Heroku

--- server/index.js
+++ server/index.js
@@ -13,15 +13,15 @@ app.listen(PORT, () => {
 });
 
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// // Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// // All other GET requests not handled before will return our React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
