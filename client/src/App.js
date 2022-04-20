7b1ff72400f881e989e538f49708fe53f5ff763d
Alaa Yahia <alaa.yahia.1995@gmail.com>
2022-04-21 01:07:13 +0300

    mininmal functioning applecation

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,138 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from 'react';
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Slider } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
 import "./App.css";
 
-function App() {
-  const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
+function App() {
+  const[pinged, setPinged] =  useState(false);
+  const[getTable, setGetTable]= useState();
+  const [data, setData] = useState([]);
+  const [url, setUrl] = useState('');
+  const [packeges, setPackeges] = useState(15);
+  const [pingResponse, setPingResponse] = useState("");
+  const [pingErrResponse, setPingErrResponse] = useState("");
+
+
+
+  /// here add a post fetch to the server with url + packeges
+  useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+      .then((data) => {setData(data);
+        setGetTable();
+      }
+      )
+      setTimeout(() => {setGetTable(true)}, 10000);
+  }, [getTable]);
 
+  
+  const handleHost = e => {
+    setUrl(e.target.value)
+  };
+
+  const handlePackets = number => {      
+    setPackeges(number)
+  };
+
+  const handleSubmit = (e) => {
+    setPinged(false);
+    setPingResponse("");
+    setPingErrResponse("");
+    fetch("/ping", {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({url: url, packeges: packeges}),
+    })
+     .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+      setPingResponse(data.pingRes);
+      setPingErrResponse(data.pingErr);
+      setGetTable(true);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+    setPinged(true);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+  };
+
+
+  const Table = () => {
+    return (
+    <table id="customers">
+      <tr>
+        <th>URL</th>
+        <th>Count</th>
+        
+      </tr>
+      {data.map(d => (
+        <tr>
+          <td>{d.url}</td>
+          <td>{d.count}</td>
+        </tr>
+        ))}
+    </table>
+    );
+  
+  }
+
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        {/* <p>{!pingResponse ? "Loading..." : pingResponse}</p>
+        <p>{!pingErrResponse ? "" : pingErrResponse}</p> */}
+         <h1 color="">Ping Form</h1>
+        <Form 
+            onFinish={handleSubmit}
+            onFinishFailed={onFinishFailed}
+            >
+            
+           
+            <p className='labeled'>Choose a host to ping with number of packeges.</p>
+            <br></br>
+            <Form.Item
+             name="host"
+            rules={[
+          {
+            required: true,
+            message: 'Please enter host url',
+          },
+        ]}>
+              <Input  onChange={handleHost} value={url} placeholder='place host URL here' style = {{ borderColor: "indianred"}}></Input>
+                {/* <TextArea rows={10} onChange={handleText} value={text} placeholder='Please write your feedback here'/> */}
+            </Form.Item>
+            <Form.Item>
+                {/* <Rate onChange={handleRate} /> */}
+                <Slider onChange={handlePackets} defaultValue={15} min={1} max={30}  trackStyle = {{ background: "indianred" }} handleStyle= {{ background: "indianred" , borderColor: "white"}} />
+            </Form.Item>
+            <br></br>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" danger ghost>
+                PING
+                </Button>
+            </Form.Item>
+            <Form.Item>
+            <TextArea rows={4} placeholder='The ping result will appear here' value={pinged && !pingResponse && !pingErrResponse ? "laodign..." : pingResponse || pingErrResponse}></TextArea>
+              </Form.Item>
+            </Form>
+            {/* {<p>{!pingResponse && !pingErrResponse ? "Loading..." : pingResponse || pingErrResponse}</p>} */}
+           
+            <p> Top Pinged Hosts </p>
+            { Table()}
+         
+              
       </header>
+
+      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,18 +1,66 @@
 const path = require('path');
 const express = require("express");
+const util = require('util')
+const exec = util.promisify(require('child_process').exec);
+const bodyParser = require('body-parser');
+const { count } = require('console');
+
+
+// ping('www.kindacode.com', 5); 
 
 const PORT = process.env.PORT || 3003;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+let hostsCount = [];
+
+///here add post handler to dothe ping and send the message back to the react.
+
+app.post("/ping", async (req, res) => {
+  try{
+  let {stdout, stderr} = await exec(`ping -c ${ req.body.packeges} ${req.body.url}`);
+  console.log("stdout: ",  stdout)
+  console.log("stderr: ",  stderr)
+  let host = hostsCount.find(c => c.url === req.body.url);
+  if(!host){
+      host = {id: hostsCount.length +1,
+      url: req.body.url,
+      count: 1
+      }
+      hostsCount.push(host);
+  }
+  else{
+  host.count += 1;
+}
+ 
+  res.json({ pingRes: stdout.toString(), pingErr: stderr.toString()});
+  }
+  catch(err){
+    res.json({ pingRes: "", pingErr: err.message});
+  }
+});
+
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  // let mes = "Hello from server!";
+  // res.json({ message: mes});
+  var top5 = hostsCount.sort(function(a, b) { return a.count < b.count ? 1 : -1; })
+                .slice(0, 5);
+
+console.log(top5);
+  res.send(top5);
 });
+
+
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
